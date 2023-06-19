1f6ab32239f63b26a935253489f774157666d40d
Visanth <vijayanvisanthr@gmail.com>
2023-06-19 14:19:05 +0530

    ADD MatToolbarModule & REFACTOR

--- src/app/shared/components/navbar/navbar.module.ts
+++ src/app/shared/components/navbar/navbar.module.ts
@@ -6,15 +6,11 @@ import { NavbarComponent } from './navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { RouterLink } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    RouterLink,
-  ],
-  exports: [NavbarComponent]
+  imports: [CommonModule, MatToolbarModule, RouterLink, MatButtonModule],
+  exports: [NavbarComponent],
 })
-export class NavbarModule { }
+export class NavbarModule {}
