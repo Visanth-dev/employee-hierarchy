dca20f3db90453ace15a051a073bd2ebeb75b686
Visanth <vijayanvisanthr@gmail.com>
2023-06-19 14:11:53 +0530

    CREATE confirmation-dialog

---
+++ src/app/pages/home/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css'],
+})
+export class ConfirmationDialogComponent {
+  @Output() confirm = new EventEmitter();
+}
